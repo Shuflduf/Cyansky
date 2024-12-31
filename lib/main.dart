b41aadcb9a147bcc250c798047655c1a93830c26
Shuflduf <shuflduf@gmail.com>
2024-12-30 18:27:23 -0700

    im going to delete everything for svelete

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
 void main() async {
-  
+  var url = Uri.http('127.0.0.1:8000', '/newacc');
+  var body = jsonEncode(
+    {'name': 'doodle', 'color': 'blue'}
+  );
+  var headers = <String, String>{
+    'Content-Type': 'application/json',
+    'Content-Length': body.length.toString(),
+  };
+  var response = await http.post(
+    url,
+    body: body,
+    headers: headers,
+  );
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
 }
