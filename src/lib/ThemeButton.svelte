d29edb3caf2ceeddf26207f07847c3ff3356f4db
Shuflduf <shuflduf@gmail.com>
2025-01-16 20:44:27 -0700

    refactor: simplify ThemeButton component by removing unnecessary rendering logic

--- src/lib/ThemeButton.svelte
+++ src/lib/ThemeButton.svelte
@@ -1,37 +1,25 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { getCookie, updateCookie } from "./cookieUtil";
   import { darkMode, updateTheme } from "$lib/stores/theme";
 
-  let render = $state(false);
   let isDarkMode = $derived($darkMode);
-
-  onMount(async () => {
-    const id = getCookie("user_id");
-    if (id) {
-      render = true;
-    }
-  });
 </script>
 
-{#if render}
-  <button
-    class="py-2 px-4 {isDarkMode
-      ? 'bg-slate-800 text-white'
-      : 'bg-slate-300'} right-0 shadow-md font-bold w-full hover:shadow-lg transition"
-    onclick={() => {
-      updateTheme(!$darkMode);
-      location.reload();
-    }}
-  >
-    <img
-      src="/images/palette.png"
-      alt="Logout"
-      class="h-6 w-6 inline-block {isDarkMode ? 'image-white' : 'brightness-0'}"
-    />
-    Theme
-  </button>
-{/if}
+<button
+  class="py-2 px-4 {isDarkMode
+    ? 'bg-slate-800 text-white'
+    : 'bg-slate-300'} right-0 shadow-md font-bold w-full hover:shadow-lg transition"
+  onclick={() => {
+    updateTheme(!$darkMode);
+    location.reload();
+  }}
+>
+  <img
+    src="/images/palette.png"
+    alt="Logout"
+    class="h-6 w-6 inline-block {isDarkMode ? 'image-white' : 'brightness-0'}"
+  />
+  Theme
+</button>
 
 <style lang="postcss">
   .image-white {
